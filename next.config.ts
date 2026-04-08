26e5381643c6fd53faee3db661af9ad44e042dfb
root <root@localhost.localdomain>
2026-04-08 23:06:57 +0800

    fix: add Google avatar domain to images config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['logan-entered-kenny-much.trycloudflare.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
